19676418e8d2d9ce807f11bdc7f0011d2b9f6760
fiersik <tltkirik@gmail.com>
2023-04-24 14:15:03 +0400

    0.0.3 [ added "Взять ёжика" function ]

--- handlers/Basic.py
+++ handlers/Basic.py
@@ -5,8 +5,26 @@ Basic_labeler = BotLabeler()
 Basic_labeler.vbml_ignore_case = True
 
 
+@Basic_labeler.chat_message(text = "взять ёжика")
+async def take_a_hedgehog(m: Message):
+
+    if db.get_info(m.from_id, f"chat_{m.peer_id}", "hedgehog") is None and db.get_info(m.peer_id, "chat_stat", "new_hedgehog") is not None:
+        hedgehog = db.get_info(m.peer_id, "chat_stat", "new_hedgehog")
+        db.update_info(m.from_id, f"chat_{m.peer_id}", "hedgehog", hedgehog)
+        db.update_info(m.peer_id, "chat_stat", "new_hedgehog", None)
+
+        await m.answer(
+            "Вы взяли ёжика"
+        )
+
+    elif db.get_info(m.from_id, f"chat_{m.peer_id}", "hedgehog") is  not None:
+        await m.answer("У вас уже есть ёжик")
+    else:
+        await m.answer("В вашей беседе нет свободного ёжика")
+
+
 @Basic_labeler.chat_message(text = "мой ёжик")
-async def hello(m: Message):
+async def my_hedgehog(m: Message):
 
     if db.get_info(m.from_id, f"chat_{m.peer_id}", "hedgehog") is None:
         await m.answer(
